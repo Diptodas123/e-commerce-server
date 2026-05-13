6191f331da3f81bca56e8efeae22a8f3b6b80bd4
Dipto-at-sap <dipto.das@sap.com>
2026-05-13 22:45:08 +0530

    Fix token issue

--- src/services/common/auth.service.js
+++ src/services/common/auth.service.js
@@ -66,10 +66,11 @@ export const authenticateUser = async (res, email, password) => {
         userName: user.userName
     });
 
+    const isProduction = process.env.NODE_ENV === "production";
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
